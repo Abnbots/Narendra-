8250895b2f3590888da93f2351da39a157ac5e3a
Abnbots <abhinavnarendra19@gmail.com>
2025-08-26 17:20:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
 CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
 
 # Database Information
-DB_URI = environ.get("DB_URI", "mongodb+srv://abhinavnarendra406Tcd7enIpdr4NOaxcluster0.y7xyrkv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = environ.get("DB_URI", "mongodb+srv://abhinavnarendra24:HLLYOwd339JIgxWe@cluster0.sbueait.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = environ.get("DB_NAME", "techvjbotz")
 
 # Auto Delete Information
